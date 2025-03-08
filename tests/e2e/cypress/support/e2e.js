10a0937a8acdff2171a998e2dd1ffda876cf9c93
Asabaal Horan <156483330+asabaal@users.noreply.github.com>
2025-03-08 14:05:00 -0600

    Convert cypress support e2e.js to use CommonJS syntax

--- tests/e2e/cypress/support/e2e.js
+++ tests/e2e/cypress/support/e2e.js
@@ -5,11 +5,8 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
-import './commands';
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+// Import commands.js using CommonJS syntax:
+require('./commands');
 
 // Hide fetch/XHR requests in the Cypress command log
 const app = window.top;
