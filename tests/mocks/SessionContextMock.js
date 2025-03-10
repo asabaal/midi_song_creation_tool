4c823bc7104444a9c89979fec5e483352c6b030f
Asabaal Horan <156483330+asabaal@users.noreply.github.com>
2025-03-09 19:58:12 -0500

    Add SessionContext mock for component tests

---
+++ tests/mocks/SessionContextMock.js
@@ -0,0 +1,31 @@
+// tests/mocks/SessionContextMock.js
+export const useSessionContext = jest.fn().mockReturnValue({
+  currentSession: {
+    id: 'test-session',
+    bpm: 120,
+    timeSignature: [4, 4],
+    tracks: [
+      {
+        id: 0,
+        name: 'Piano',
+        instrument: 0,
+        notes: [
+          { id: 'note1', pitch: 60, startTime: 0, duration: 1, velocity: 100 },
+          { id: 'note2', pitch: 64, startTime: 1, duration: 1, velocity: 100 },
+          { id: 'note3', pitch: 67, startTime: 2, duration: 1, velocity: 100 },
+        ]
+      }
+    ],
+    loop: {
+      enabled: false,
+      start: 0,
+      end: 16
+    }
+  },
+  addNote: jest.fn(),
+  updateNote: jest.fn(),
+  deleteNote: jest.fn(),
+  selectedTrackId: 0,
+  setSelectedTrackId: jest.fn(),
+  updateTransport: jest.fn()
+});
