6bd7082269c689b7f206b34563ebdce184e43d2b
Asabaal Horan <156483330+asabaal@users.noreply.github.com>
2025-03-11 20:31:28 -0500

    Fix Jest configuration conflict
    - Remove Jest configuration from package.json
    - Create explicit jest.config.js file
    - Add required mock files for CSS and static assets
    - Update test script to explicitly use the config file

--- jest.config.js
+++ jest.config.js
@@ -1,54 +1,35 @@
+// jest.config.js
 module.exports = {
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
-
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
-
-  // A list of paths to directories that Jest should use to search for files in
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-
-  // The test environment that will be used for testing
+  // Setup environment
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  
+  // Test timeout
+  testTimeout: 15000,
+  
+  // Test environment
   testEnvironment: 'jsdom',
-
-  // Setup files that will be run before each test
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-
-  // An array of regexp pattern strings that are matched against all test paths
-  testPathIgnorePatterns: ['/node_modules/', '/cypress/'],
-
-  // A map from regular expressions to module names or to arrays of module names
-  moduleNameMapper: {
-    // Handle CSS/SCSS files
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    // Handle image imports
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-    // Path aliases (if applicable)
-    '^@/(.*)$': '<rootDir>/src/$1',
-    // Explicit mock for SessionContext
-    '^src/client/context/SessionContext$': '<rootDir>/__mocks__/src/client/context/SessionContext.js'
-  },
-
-  // An array of regexp pattern strings that are matched against all file paths
-  transformIgnorePatterns: ['/node_modules/(?!tone)/'],
-
-  // Watchman plugins for filtering tests
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname'
+  
+  // Coverage configuration
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.test.{js,jsx}',
+    '!src/index.js',
+    '!src/server/server.js'
   ],
-
-  // Avoid test runs that fail due to memory issues in large projects
-  workerIdleMemoryLimit: '512MB',
   
-  // Mock resolution
-  moduleDirectories: ['node_modules', '<rootDir>'],
+  // Transform files
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
   
-  // Automatically restore mock state between every test
-  restoreMocks: true,
+  // Module file extensions
+  moduleFileExtensions: ['js', 'jsx', 'json'],
+  
+  // Mock file extensions
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/tests/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/tests/__mocks__/fileMock.js'
+  },
   
   // Indicates whether each individual test should be reported during the run
   verbose: true
