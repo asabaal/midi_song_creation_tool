1e730bbbc9b2f53e64a48cbdbfd7d9d323e542cc
Asabaal Horan <156483330+asabaal@users.noreply.github.com>
2025-03-11 19:05:06 -0500

    Add ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,67 +1,52 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
-    es2021: true,
     node: true,
+    es2021: true,
     jest: true,
   },
   extends: [
     'eslint:recommended',
-    'prettier' // Add prettier last to override conflicting rules
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
+    'prettier'
   ],
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
-  },
-  rules: {
-    // Error prevention
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'no-console': 'warn',
-    'no-undef': 'error',
-    
-    // MIDI specific rules to ensure pattern correctness
-    'max-len': ['warn', { code: 100, ignoreComments: true }],
-    
-    // Add prettier as a rule (will use your .prettierrc settings)
-    'prettier/prettier': ['error']
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
   plugins: [
-    'prettier' // Add prettier plugin
+    'react',
+    'import',
+    'jsx-a11y',
+    'prettier'
   ],
-  // Jest test file specific relaxations
-  overrides: [
-    {
-      files: ['**/*.test.js', '**/*.spec.js', '**/*.test.jsx', '**/*.spec.jsx'],
-      rules: {
-        'no-console': 'off',
-      }
-    },
-    // JSX files
-    {
-      files: ['**/*.jsx'],
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true
-        }
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'react/prop-types': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
       },
-      globals: {
-        React: 'readonly',
-        ReactDOM: 'readonly'
-      }
-    }
-  ],
-  globals: {
-    'expect': 'readonly',
-    'describe': 'readonly',
-    'it': 'readonly',
-    'test': 'readonly',
-    'beforeEach': 'readonly',
-    'afterEach': 'readonly',
-    'beforeAll': 'readonly',
-    'afterAll': 'readonly',
-  }
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
 };
