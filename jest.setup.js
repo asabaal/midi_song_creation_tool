6ff0a8e8a669f60d1a203df7e3e441f7e50edcab
Asabaal Horan <156483330+asabaal@users.noreply.github.com>
2025-03-12 05:47:39 -0500

    Update jest.setup.js to include testing-library extensions

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,5 @@
 // jest.setup.js
+import '@testing-library/jest-dom';
 
 // Polyfill for setImmediate and clearImmediate
 if (typeof global.setImmediate !== 'function') {
@@ -18,3 +19,27 @@ if (typeof global.TextEncoder === 'undefined') {
 
 // This line helps with act() warnings in React tests
 global.IS_REACT_ACT_ENVIRONMENT = true;
+
+// Mock ResizeObserver which isn't available in jsdom
+class ResizeObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+window.ResizeObserver = ResizeObserverMock;
+
+// Mock Audio Context
+window.AudioContext = jest.fn().mockImplementation(() => ({
+  createOscillator: jest.fn().mockReturnValue({
+    connect: jest.fn(),
+    start: jest.fn(),
+    stop: jest.fn(),
+    frequency: { value: 0 }
+  }),
+  createGain: jest.fn().mockReturnValue({
+    connect: jest.fn(),
+    gain: { value: 0 }
+  }),
+  destination: {}
+}));
