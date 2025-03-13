4507febcb0efd2066fa66d97b9efef070a0af8ff
Asabaal Horan <156483330+asabaal@users.noreply.github.com>
2025-03-12 19:31:38 -0500

    Remove duplicate mock files completely

--- tests/__mocks__/fileMock.js
+++
@@ -1,3 +0,0 @@
-// tests/__mocks__/fileMock.js
-// This is a placeholder file to avoid deleting the directory structure.
-// The actual mock is in the root __mocks__ directory.
--- tests/__mocks__/styleMock.js
+++
@@ -1,3 +0,0 @@
-// tests/__mocks__/styleMock.js
-// This is a placeholder file to avoid deleting the directory structure.
-// The actual mock is in the root __mocks__ directory.
