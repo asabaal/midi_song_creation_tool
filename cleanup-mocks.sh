b7ce34b639fd0ef158d5f32b81f076c36e5f38cb
Asabaal Horan <asabaalhoran@gmail.com>
2025-03-15 12:58:12 -0500

    removing more tuff

--- cleanup-mocks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Remove all mock files from tests directory
-rm -rf tests/__mocks__
-
-echo "Tests mock directory removed. Using only root level mocks now."
--- remove-duplicate-mocks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Remove the duplicate mock files
-rm -f tests/__mocks__/fileMock.js
-rm -f tests/__mocks__/styleMock.js
-
-# Report successful removal
-echo "Duplicate mock files removed successfully."
